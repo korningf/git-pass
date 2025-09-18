35c827247153aff1a98f7e361ff45d63b7517809
Francis Korning <fkorning@outlook.com>
2025-09-18 13:55:51 +0100

    Add files via upload
    inception.

---
+++ install.sh
@@ -0,0 +1,46 @@
+#
+
+PLATFORM=`uname | cut -d _ -f 1 | tr '[:upper:]' '[:lower:]'`
+# for cygwin
+PLATFORM=cygwin
+# for gitbash, sysgit, msys2, msys
+PLATFORM=mingw64
+
+
+#DESTDIR=C:
+DESTDIR=
+PREFIX=/usr
+BINDIR=${PREFIX}/bin
+LIBDIR=${PREFIX}/lib
+MANDIR=${PREFIX}/share/man
+
+
+PLATFORMFILE=src/platform/${PLATFORM}.sh
+
+
+# install manuals
+install-manuals () {
+	install -v -d "${DESTDIR}${MANDIR}/man1" && install -m 0644 -v man/pass.1 "${DESTDIR}${MANDIR}/man1/pass.1"
+}
+
+
+# install default pass
+install-default () {
+	install -v -d "${DESTDIR}${LIBDIR}/password-store/extensions"
+	trap 'rm -f src/.pass' EXIT; sed '/PLATFORM_FUNCTION_FILE/d;s:^SYSTEM_EXTENSION_DIR=.*:SYSTEM_EXTENSION_DIR="${LIBDIR}/password-store/extensions":' src/password-store.sh > src/.pass && \
+	install -v -d "${DESTDIR}${BINDIR}/" && install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
+}
+
+# install platform patch
+install-platform () {
+	install -v -d "${DESTDIR}${LIBDIR}/password-store" && install -m 0644 -v "${PLATFORMFILE}" "${DESTDIR}${LIBDIR}/password-store/platform.sh"
+	install -v -d "${DESTDIR}${LIBDIR}/password-store/extensions"
+	install -v -d "${DESTDIR}${BINDIR}/"
+	trap 'rm -f src/.pass' EXIT; sed 's:.*PLATFORM_FUNCTION_FILE.*:source "${LIBDIR}/password-store/platform.sh":;s:^SYSTEM_EXTENSION_DIR=.*:SYSTEM_EXTENSION_DIR="${LIBDIR}/password-store/extensions":' src/password-store.sh > src/.pass && \
+	install -v -d "${DESTDIR}${BINDIR}/" && install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
+}
+
+install-manuals
+install-platform
+
+
