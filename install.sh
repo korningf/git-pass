ab51bd803b1b012f5de23242d2b756fd03d7178c
FrancisKorning <FrancisKorning@welfare.ie>
2025-09-18 16:41:20 +0100

    added gnuwin32 tree

--- install.sh
+++ install.sh
@@ -18,29 +18,36 @@ MANDIR=${PREFIX}/share/man
 PLATFORMFILE=src/platform/${PLATFORM}.sh
 
 
-# install manuals
-install-manuals () {
-	install -v -d "${DESTDIR}${MANDIR}/man1" && install -m 0644 -v man/pass.1 "${DESTDIR}${MANDIR}/man1/pass.1"
+# install common
+install-common () {
+	install -v -d "${DESTDIR}${BINDIR}/"
+	install -v -d "${DESTDIR}${LIBDIR}/password-store"
+	install -v -d "${DESTDIR}${LIBDIR}/password-store/extensions"	
+	install -v -d "${DESTDIR}${MANDIR}/man1" 
+	install -m 0644 -v man/pass.1 "${DESTDIR}${MANDIR}/man1/pass.1"
+	install -m 0644 -v man/tree.1 "${DESTDIR}${MANDIR}/man1/tree.1"
 }
 
 
 # install default pass
 install-default () {
-	install -v -d "${DESTDIR}${LIBDIR}/password-store/extensions"
 	trap 'rm -f src/.pass' EXIT; sed '/PLATFORM_FUNCTION_FILE/d;s:^SYSTEM_EXTENSION_DIR=.*:SYSTEM_EXTENSION_DIR="${LIBDIR}/password-store/extensions":' src/password-store.sh > src/.pass && \
-	install -v -d "${DESTDIR}${BINDIR}/" && install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
+	install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
 }
 
 # install platform patch
 install-platform () {
-	install -v -d "${DESTDIR}${LIBDIR}/password-store" && install -m 0644 -v "${PLATFORMFILE}" "${DESTDIR}${LIBDIR}/password-store/platform.sh"
-	install -v -d "${DESTDIR}${LIBDIR}/password-store/extensions"
-	install -v -d "${DESTDIR}${BINDIR}/"
+	install -m 0644 -v "${PLATFORMFILE}" "${DESTDIR}${LIBDIR}/password-store/platform.sh"
 	trap 'rm -f src/.pass' EXIT; sed 's:.*PLATFORM_FUNCTION_FILE.*:source "${LIBDIR}/password-store/platform.sh":;s:^SYSTEM_EXTENSION_DIR=.*:SYSTEM_EXTENSION_DIR="${LIBDIR}/password-store/extensions":' src/password-store.sh > src/.pass && \
-	install -v -d "${DESTDIR}${BINDIR}/" && install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
+	install -m 0755 -v src/.pass "${DESTDIR}${BINDIR}/pass"
 }
 
-install-manuals
-install-platform
+# install binaries
+install-binaries () {
+	install -m 0755 -v bin/tree.exe "${DESTDIR}${BINDIR}/tree.exe"
+}
 
+install-common
+install-platform
+install-binaries
 
